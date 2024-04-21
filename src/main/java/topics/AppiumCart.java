98147944029c1c35cd53e022ecf71048d50cb6fd
aviguptamra1990 <avi.guptaautomation@gmail.com>
2024-04-21 13:02:58 +0530

    replace with Appium Cart class

--- src/main/java/topics/AppiumCart.java
+++ src/main/java/topics/AppiumCart.java
@@ -5,7 +5,7 @@ public class AppiumCart {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Appium Cart");
+		System.out.println("Appium Cart class");
 
 	}
 
