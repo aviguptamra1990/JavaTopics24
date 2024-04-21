93ebf4c5c534261e19dbb077b7e61da24a74fc1e
aviguptamra1990 <avi.guptaautomation@gmail.com>
2024-04-22 02:54:39 +0530

    Revert "Update AppiumCart.java"
    This reverts commit b4994f4f6dfccfcac7d6a90960a29949aea4303e.

--- src/main/java/topics/AppiumCart.java
+++ src/main/java/topics/AppiumCart.java
@@ -5,7 +5,7 @@ public class AppiumCart {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("New Selenium Cart class from Avi Gupta");
+		System.out.println("New Selenium Cart class");
 
 	}
 
